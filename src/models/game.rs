745434e87dbea2e27396b2050ad0d9898bb8c086
João Xavier <fastjonh@gmail.com>
2024-09-07 20:08:28 -0300

    using the new iterator and tidying things up

--- src/models/game.rs
+++ src/models/game.rs
@@ -4,36 +4,43 @@ use indexmap::IndexMap;
 
 use crate::models::GameError;
 
-use super::{BiddingError, BiddingRound, Card, DealingMode, GameEvent, Player, Turn, TurnError};
+use super::{
+    iter::CyclicIterator, BiddingError, Card, DealingMode, GameState, Player, RoundInfo,
+    RoundState, Turn, TurnError,
+};
 
 #[derive(Debug)]
-pub struct Game {
+pub struct Game<'a> {
     decks: IndexMap<String, Player>,
-    hand_index: usize,
-    current_player_index: usize,
     round_cards: BinaryHeap<Turn>,
     dealing_mode: DealingMode,
-    current_cards_count: usize,
+    cyclic: CyclicIterator<'a, String>,
+    cards_count: usize,
+}
+
+#[derive(PartialEq, Debug)]
+enum CycleStage {
+    Dealing,
+    Bidding,
 }
 
 const MAX_AVAILABLE_CARDS: usize = 40 - 1;
 const MAX_PLAYER_COUNT: usize = 10;
 
-impl Game {
+impl<'a> Game<'a> {
     pub fn new(players: Vec<String>) -> Result<Self, GameError> {
-        const INITIAL_HAND_INDEX: usize = 0;
-
         validate_game(&players)?;
 
-        let decks = Self::get_decks(&players, INITIAL_HAND_INDEX + 1);
+        let initial_cards_count = 1;
+
+        let decks = Self::get_decks(&players, initial_cards_count);
 
         Ok(Self {
             decks,
             round_cards: BinaryHeap::new(),
-            current_player_index: INITIAL_HAND_INDEX,
-            hand_index: INITIAL_HAND_INDEX,
             dealing_mode: DealingMode::Increasing,
-            current_cards_count: 1,
+            cards_count: initial_cards_count,
+            cyclic: CyclicIterator::new(players),
         })
     }
 
@@ -44,47 +51,94 @@ impl Game {
             .collect()
     }
 
-    pub fn advance(&mut self, turn: Turn) -> Result<GameEvent, TurnError> {
-        let current_player_id = self.get_current_player_id();
-
-        if current_player_id != turn.player_id {
-            return Err(TurnError::NotYourTurn);
+    pub fn deal(&mut self, turn: Turn) -> Result<(RoundInfo, GameState), TurnError> {
+        if self.get_cycle_stage() == CycleStage::Bidding {
+            return Err(TurnError::BiddingStageActive);
         }
 
-        let player = &self.decks[&current_player_id];
+        let player = self
+            .decks
+            .get_mut(&turn.player_id)
+            .ok_or(TurnError::InvalidPlayer)?;
+
+        let current_dealer = self.cyclic.peek();
+
+        if current_dealer != Some(&turn.player_id) {
+            return Err(TurnError::NotYourTurn);
+        }
 
         if !player.deck.contains(&turn.card) {
             return Err(TurnError::NotYourCard);
         }
 
-        if self.decks.values().any(|p| p.bid.is_none()) {
-            return Err(TurnError::PlayersNotBidded);
-        }
+        player.deck.retain(|&c| c != turn.card);
 
-        if self.decks.len() == 1 {
-            let first = self.decks.first().expect("Should contain one");
-            return Ok(GameEvent::GameEnded(first.0.to_string()));
-        }
-
-        self.current_player_index += 1;
+        //add card to the heap
         self.round_cards.push(turn);
 
+        // finish round
         if self.round_cards.len() == self.decks.len() {
-            self.remove_lifes();
-            self.remove_losers();
-            self.start_new_round();
-            // todo needs to adjust the self.current_player_index and hand_index when a player is
-            //removed
+            let winner = self
+                .round_cards
+                .iter()
+                .next()
+                .expect("Should contain a turn");
+
+            self.award_points(winner.clone());
         }
 
-        Ok(GameEvent::TurnPlayed)
+        //finish set
+        if self.decks.iter().all(|(_, p)| p.deck.is_empty()) {
+            // todo send message when player loses a life
+            self.remove_lifes();
+            // todo send message when player loses the game
+            self.remove_losers();
+            self.start_new_set();
+        }
+
+        let game_state = if self.decks.len() == 1 {
+            let first = self.decks.first().expect("Should contain one");
+            GameState::Ended {
+                winner: first.0.to_string(),
+            }
+        } else {
+            GameState::Running
+        };
+
+        Ok((self.get_round_state(), game_state))
     }
 
-    pub fn bid(&mut self, player_id: &str, bid: usize) -> Result<BiddingRound, BiddingError> {
+    fn get_round_state(&mut self) -> RoundInfo {
+        self.cyclic.next();
+
+        match self.cyclic.peek() {
+            Some(n) => RoundInfo::new(n.clone(), RoundState::Active),
+            None => {
+                let next = self.cyclic.reset();
+                RoundInfo::new(next, RoundState::Ended)
+            }
+        }
+    }
+
+    pub fn bid(&mut self, player_id: &String, bid: usize) -> Result<RoundInfo, BiddingError> {
+        if self.get_cycle_stage() == CycleStage::Dealing {
+            return Err(BiddingError::DealingStageActive);
+        }
+
+        if bid > self.cards_count {
+            return Err(BiddingError::BidOutOfRange);
+        }
+
         let player = self
             .decks
             .get_mut(player_id)
             .ok_or(BiddingError::InvalidPlayer)?;
+
+        let current_bidder = self.cyclic.peek();
+
+        if Some(player_id) != current_bidder {
+            return Err(BiddingError::NotYourTurn);
+        }
 
         if player.bid.is_some() {
             return Err(BiddingError::AlreadyBidded);
@@ -92,39 +146,22 @@ impl Game {
 
         player.bid = Some(bid);
 
-        Ok(todo!("Need to implement a system to handle the bidding loop and to get the next player to bid"))
+        Ok(self.get_round_state())
     }
 
-    pub fn next_bidder(&mut self) -> Option<String> {
-        todo!()
-    }
-
-    fn get_current_player_id(&mut self) -> String {
-        if self.current_player_index == self.decks.len() {
-            self.current_player_index = 0;
+    fn get_cycle_stage(&mut self) -> CycleStage {
+        match self.decks.values().any(|p| p.bid.is_none()) {
+            true => CycleStage::Bidding,
+            false => CycleStage::Dealing,
         }
-
-        let player = self
-            .decks
-            .get_index(self.current_player_index)
-            .expect("Should have this index");
-
-        player.0.to_string()
     }
 
-    fn start_new_round(&mut self) {
-        self.hand_index += 1;
-
-        if self.hand_index == self.decks.len() {
-            self.hand_index = 0;
-        }
-
-        self.current_player_index = self.hand_index;
-
+    fn start_new_set(&mut self) {
         let (mode, count) =
-            Self::get_new_cards_mode(self.dealing_mode, self.hand_index, self.decks.len());
+            Self::get_new_cards_mode(self.dealing_mode, self.cards_count, self.decks.len());
 
         self.dealing_mode = mode;
+        self.cards_count = count;
 
         let players: Vec<_> = self.decks.keys().cloned().collect();
 
@@ -177,6 +214,15 @@ impl Game {
     fn remove_losers(&mut self) {
         self.decks.retain(|_, p| p.lifes != 0)
     }
+
+    fn award_points(&mut self, turn: Turn) {
+        let player = self
+            .decks
+            .get_mut(&turn.player_id)
+            .expect("This player should exist here");
+
+        player.rounds += 1;
+    }
 }
 
 fn validate_game(players: &[String]) -> Result<(), GameError> {
@@ -203,8 +249,6 @@ mod tests {
         let mut game = Game::new(vec![player1.clone(), player2.clone()]).unwrap();
 
         assert!(game.round_cards.is_empty());
-        assert!(game.current_player_index == 0);
-        assert!(game.hand_index == 0);
 
         let first_played_card = game.decks[&player1].deck[0];
         let first_turn = Turn {
@@ -213,12 +257,13 @@ mod tests {
         };
 
         game.bid(&player1, 1).unwrap();
-        game.bid(&player2, 2).unwrap();
+        game.bid(&player2, 1).unwrap();
 
-        game.advance(first_turn).unwrap();
+        println!("Pre deal 1 | {game:?}");
+
+        game.deal(first_turn).unwrap();
 
         assert!(game.round_cards.len() == 1);
-        assert!(game.current_player_index == 1);
         assert!(game.round_cards.peek().map(|t| t.card) == Some(first_played_card));
 
         let second_played_card = game.decks[&player2].deck[0];
@@ -227,12 +272,11 @@ mod tests {
             card: second_played_card,
         };
 
-        game.advance(second_turn).unwrap();
+        println!("Pre deal 2 | {game:?}");
+
+        game.deal(second_turn).unwrap();
 
         assert!(game.round_cards.len() == 2);
-        assert!(game.current_player_index == 1);
-
-        assert!(game.hand_index == 1);
     }
 
     #[test]
