0e24f46178535c4eb7ec8c8575355d65b139250f
João Xavier <fastjonh@gmail.com>
2024-09-06 23:24:54 -0300

    adding more game steps to the int test

--- tests/int_test.rs
+++ tests/int_test.rs
@@ -6,7 +6,7 @@ mod tests {
     use mongodb::bson::oid::ObjectId;
     use oh_hell::{
         infra::{
-            auth::{ProfileParams, TokenResponse},
+            auth::{get_claims_from_token, ProfileParams, TokenResponse},
             lobby::CreateLobbyResponse,
             ClientGameMessage, ClientMessage, JoinLobbyDto, ServerMessage,
         },
@@ -20,52 +20,78 @@ mod tests {
 
     #[tokio::test]
     async fn test_example() {
-        let start_app = oh_hell::start_app();
-        let app = start_app;
-
-        task::spawn(app);
+        task::spawn(oh_hell::start_app());
 
         let mut client = reqwest::Client::new();
 
-        let p1_t = login(&mut client).await;
-        let p2_t = login(&mut client).await;
+        let p1_token = login(&mut client).await;
+        let p2_token = login(&mut client).await;
 
-        let lobby_id = create_lobby(&mut client, &p1_t).await;
+        let p1_claims = get_claims_from_token(&p1_token).await.unwrap();
+        let p2_claims = get_claims_from_token(&p2_token).await.unwrap();
 
-        let lobby = join_lobby(&mut client, &p1_t, lobby_id).await;
+        let lobby_id = create_lobby(&mut client, &p1_token).await;
+
+        let lobby = join_lobby(&mut client, &p1_token, lobby_id).await;
         assert!(lobby.players.len() == 1);
 
-        let lobby = join_lobby(&mut client, &p2_t, lobby_id).await;
+        let lobby = join_lobby(&mut client, &p2_token, lobby_id).await;
         assert!(lobby.players.len() == 2);
 
-        let mut p1_s = connect_ws(p1_t).await;
-        let mut p2_s = connect_ws(p2_t).await;
+        let mut p1 = connect_ws(p1_token).await;
+        let mut p2 = connect_ws(p2_token).await;
 
-        let ready = ClientGameMessage::PlayerStatusChange { ready: true };
+        let msg = ClientGameMessage::PlayerStatusChange { ready: true };
+        send_msg(&mut p1, msg).await;
+        send_msg(&mut p2, msg).await;
 
-        send_msg(&mut p1_s, ready).await;
-        send_msg(&mut p2_s, ready).await;
+        //p2 receives himself and p1 ready
+        assert_game_msg(&mut p1, validate_player_status_change).await;
+        assert_game_msg(&mut p2, validate_player_status_change).await;
+        assert_game_msg(&mut p2, validate_player_status_change).await;
 
-        let player_ready_predicate = |m: &ServerMessage| {
-            matches!(
-                m,
-                ServerMessage::PlayerStatusChange {
-                    player_id: _,
-                    ready: _
-                }
-            )
-        };
-
-        //p1 receives himself and p2 ready
-        assert_game_msg(&mut p1_s, player_ready_predicate).await;
-        assert_game_msg(&mut p1_s, player_ready_predicate).await;
-        assert_game_msg(&mut p2_s, player_ready_predicate).await;
-
-        let p1_deck = get_deck(&mut p1_s).await;
+        let p1_deck = get_deck(&mut p1).await;
+        let p2_deck = get_deck(&mut p2).await;
         assert!(p1_deck.len() == 1);
-
-        let p2_deck = get_deck(&mut p2_s).await;
         assert!(p2_deck.len() == 1);
+
+        assert_game_msg(&mut p1, get_bidding_turn_predicate(p1_claims.id())).await;
+        let msg = ClientGameMessage::PutBid { bid: 0 };
+        send_msg(&mut p1, msg).await;
+
+        assert_game_msg(&mut p2, get_bidding_turn_predicate(p2_claims.id())).await;
+        send_msg(&mut p2, msg).await;
+
+        assert_game_msg(&mut p1, validate_player_turn).await;
+        assert_game_msg(&mut p2, validate_player_turn).await;
+
+        let msg = ClientGameMessage::PlayTurn { card: p1_deck[0] };
+        send_msg(&mut p1, msg).await;
+
+        assert_game_msg(&mut p1, validate_turn_played).await;
+        assert_game_msg(&mut p2, validate_turn_played).await;
+    }
+
+    fn validate_turn_played(m: &ServerMessage) -> bool {
+        matches!(m, ServerMessage::TurnPlayed { turn: _ })
+    }
+
+    fn validate_player_turn(m: &ServerMessage) -> bool {
+        matches!(m, ServerMessage::PlayerTurn { player_id: _ })
+    }
+
+    fn get_bidding_turn_predicate(id: String) -> impl FnOnce(&ServerMessage) -> bool {
+        |m: &ServerMessage| m == &ServerMessage::PlayerBiddingTurn { player_id: id }
+    }
+
+    fn validate_player_status_change(m: &ServerMessage) -> bool {
+        matches!(
+            m,
+            ServerMessage::PlayerStatusChange {
+                player_id: _,
+                ready: _
+            }
+        )
     }
 
     async fn get_deck(stream: &mut WebSocket) -> Vec<Card> {
@@ -77,7 +103,7 @@ mod tests {
 
     async fn assert_game_msg<F>(stream: &mut WebSocket, predicate: F) -> ServerMessage
     where
-        F: Fn(&ServerMessage) -> bool,
+        F: FnOnce(&ServerMessage) -> bool,
     {
         let msg = recv_msg(stream).await;
 
