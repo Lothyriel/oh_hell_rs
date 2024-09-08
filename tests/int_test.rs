7033b0b587f68a5f2cc479a176e16444787b1cd3
João Xavier <fastjonh@gmail.com>
2024-09-08 12:26:43 -0300

    adapting tests to run for n players

--- tests/int_test.rs
+++ tests/int_test.rs
@@ -23,26 +23,39 @@ mod tests {
 
         // TODO make this test work for 2-10 players
 
-        let p1_token = login(&mut client).await;
-        let p2_token = login(&mut client).await;
+        let tokens = get_players(&mut client, 2).await;
 
-        let (mut p1, mut p2) = join_lobby(&mut client, p1_token, p2_token).await;
+        let mut connections = join_lobby(&mut client, &tokens).await;
 
-        ready(&mut p1, &mut p2).await;
+        ready(&mut connections).await;
 
         let mut cards_count = 1;
 
         loop {
-            let (p1_deck, p2_deck) = decks(&mut p1, &mut p2, cards_count).await;
+            let decks = decks(&mut connections, cards_count).await;
+
+            // todo need to loop cycle the players between sets
+            play_set(&mut connections, decks, cards_count).await;
 
             cards_count += 1;
 
-            play_set(&mut p1, &mut p2, &p1_deck, &p2_deck).await;
-
-            if assert_game_or_set_ended(&mut p1).await & assert_game_or_set_ended(&mut p2).await {
-                break;
+            for p in &mut connections {
+                if assert_game_or_set_ended(p).await {
+                    return;
+                }
             }
         }
+    }
+
+    async fn get_players(client: &mut Client, count: usize) -> Vec<String> {
+        let mut players = vec![];
+
+        for i in 0..count {
+            let player = login(client, i).await;
+            players.push(player);
+        }
+
+        players
     }
 
     async fn assert_game_or_set_ended(socket: &mut WebSocket) -> bool {
@@ -66,103 +79,108 @@ mod tests {
         }
     }
 
-    async fn play_set(p1: &mut WebSocket, p2: &mut WebSocket, p1_deck: &Deck, p2_deck: &Deck) {
-        let rounds_count = p1_deck.len();
-
-        bidding(p1, p2, rounds_count).await;
+    async fn play_set(players: &mut Vec<WebSocket>, decks: Vec<Deck>, rounds_count: usize) {
+        bidding(players, rounds_count).await;
 
         for i in 0..rounds_count {
-            play_round(p1, p2, p1_deck, p2_deck, i == rounds_count - 1).await;
+            play_round(players, &decks, i == rounds_count - 1).await;
         }
     }
 
     type Deck = Vec<Card>;
 
-    async fn play_round(
-        p1: &mut WebSocket,
-        p2: &mut WebSocket,
-        p1_deck: &Deck,
-        p2_deck: &Deck,
-        last: bool,
-    ) {
-        assert_game_msg(p1, validate_player_turn).await;
-        assert_game_msg(p2, validate_player_turn).await;
-
-        let msg = ClientGameMessage::PlayTurn { card: p1_deck[0] };
-        send_msg(p1, msg).await;
-
-        assert_game_msg(p1, validate_turn_played).await;
-        assert_game_msg(p2, validate_turn_played).await;
-
-        assert_game_msg(p1, validate_player_turn).await;
-        assert_game_msg(p2, validate_player_turn).await;
-
-        let msg = ClientGameMessage::PlayTurn { card: p2_deck[0] };
-        send_msg(p2, msg).await;
-
-        assert_game_msg(p1, validate_turn_played).await;
-        assert_game_msg(p2, validate_turn_played).await;
+    async fn play_round(players: &mut [WebSocket], decks: &[Deck], last: bool) {
+        for i in 0..players.len() {
+            play_turn(players, decks[i][0], i).await;
+        }
 
         if !last {
-            assert_game_msg(p1, validate_round_ended).await;
-            assert_game_msg(p2, validate_round_ended).await;
+            for p in players.iter_mut() {
+                assert_game_msg(p, validate_round_ended).await;
+            }
         }
     }
 
-    async fn bidding(p1: &mut WebSocket, p2: &mut WebSocket, bid: usize) {
-        bid_turn(bid, p1, p2).await;
-        bid_turn(bid, p1, p2).await;
+    async fn play_turn(players: &mut [WebSocket], deck: Card, index: usize) {
+        for p in players.iter_mut() {
+            assert_game_msg(p, validate_player_turn).await;
+        }
+
+        let msg = ClientGameMessage::PlayTurn { card: deck };
+
+        send_msg(&mut players[index], msg).await;
+
+        for p in players.iter_mut() {
+            assert_game_msg(p, validate_turn_played).await;
+        }
     }
 
-    async fn bid_turn(bid: usize, p1: &mut WebSocket, p2: &mut WebSocket) {
-        let msg = ClientGameMessage::PutBid { bid };
-
-        assert_game_msg(p1, validate_bidding_turn).await;
-        assert_game_msg(p2, validate_bidding_turn).await;
-
-        send_msg(p1, msg).await;
-
-        assert_game_msg(p1, validate_player_bidded).await;
-        assert_game_msg(p2, validate_player_bidded).await;
+    async fn bidding(players: &mut Vec<WebSocket>, bid: usize) {
+        for i in 0..players.len() {
+            bid_turn(players, i, bid).await;
+        }
     }
 
-    async fn decks(p1: &mut WebSocket, p2: &mut WebSocket, cards_count: usize) -> (Deck, Deck) {
-        assert_game_msg(p1, validate_set_start).await;
-        assert_game_msg(p2, validate_set_start).await;
+    async fn bid_turn(players: &mut Vec<WebSocket>, index: usize, bid: usize) {
+        for p in players.iter_mut() {
+            assert_game_msg(p, validate_bidding_turn).await;
+        }
 
-        let p1_deck = get_deck(p1).await;
-        let p2_deck = get_deck(p2).await;
-        assert!(p1_deck.len() == cards_count);
-        assert!(p2_deck.len() == cards_count);
+        send_msg(&mut players[index], ClientGameMessage::PutBid { bid }).await;
 
-        (p1_deck, p2_deck)
+        for p in players {
+            assert_game_msg(p, validate_player_bidded).await;
+        }
     }
 
-    async fn join_lobby(client: &mut Client, p1: String, p2: String) -> (WebSocket, WebSocket) {
-        let lobby_id = create_lobby(client, &p1).await;
+    async fn decks(players: &mut Vec<WebSocket>, cards_count: usize) -> Vec<Deck> {
+        for p in players.iter_mut() {
+            assert_game_msg(p, validate_set_start).await;
+        }
 
-        let lobby = join_lobby_http(client, &p1, lobby_id.clone()).await;
-        assert!(lobby.players.len() == 1);
+        let mut decks = vec![];
 
-        let lobby = join_lobby_http(client, &p2, lobby_id).await;
-        assert!(lobby.players.len() == 2);
+        for p in players {
+            let deck = get_deck(p).await;
 
-        let p1 = connect_ws(p1).await;
-        let p2 = connect_ws(p2).await;
+            assert!(deck.len() == cards_count);
 
-        (p1, p2)
+            decks.push(deck);
+        }
+
+        decks
     }
 
-    async fn ready(p1: &mut WebSocket, p2: &mut WebSocket) {
+    async fn join_lobby(client: &mut Client, players: &[String]) -> Vec<WebSocket> {
+        let lobby_id = create_lobby(client, &players[0]).await;
+
+        for (i, p) in players.iter().enumerate() {
+            let lobby = join_lobby_http(client, p, &lobby_id).await;
+            assert!(lobby.players.len() == i + 1);
+        }
+
+        let mut connections = vec![];
+
+        for p in players {
+            connections.push(connect_ws(p.clone()).await);
+        }
+
+        connections
+    }
+
+    async fn ready(players: &mut [WebSocket]) {
         let msg = ClientGameMessage::PlayerStatusChange { ready: true };
 
-        send_msg(p1, msg).await;
-        send_msg(p2, msg).await;
+        for p in players.iter_mut() {
+            send_msg(p, msg).await;
+        }
 
-        assert_game_msg(p1, validate_player_status_change).await;
-        assert_game_msg(p1, validate_player_status_change).await;
-        assert_game_msg(p2, validate_player_status_change).await;
-        assert_game_msg(p2, validate_player_status_change).await;
+        for i in 0..players.len() {
+            for _ in 0..players.len() {
+                let player = &mut players[i];
+                assert_game_msg(player, validate_player_status_change).await;
+            }
+        }
     }
 
     fn validate_round_ended(m: &ServerMessage) -> bool {
@@ -262,7 +280,7 @@ mod tests {
         msg
     }
 
-    async fn join_lobby_http(client: &mut Client, token: &str, lobby_id: String) -> JoinLobbyDto {
+    async fn join_lobby_http(client: &mut Client, token: &str, lobby_id: &str) -> JoinLobbyDto {
         let res = client
             .put(format!("{URL}lobby/{lobby_id}"))
             .bearer_auth(token)
@@ -286,10 +304,10 @@ mod tests {
         res.lobby_id
     }
 
-    async fn login(client: &mut Client) -> String {
+    async fn login(client: &mut Client, number: usize) -> String {
         let params = ProfileParams {
             picture: "picture.jpg".to_string(),
-            nickname: "JX".to_string(),
+            nickname: format!("Player {number}"),
         };
 
         let res = client
